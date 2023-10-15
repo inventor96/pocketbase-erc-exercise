645fb0e8c706bae623dd9c39387f9e1b2be02a37
inventor96 <inventor96@gmail.com>
2023-10-15 02:22:09 -0600

    Fixed resource-choosing logic, minimized item duplication

--- pb_hooks/assignments.pb.js
+++ pb_hooks/assignments.pb.js
@@ -6,18 +6,11 @@ onModelBeforeUpdate((e) => {
 
 	// setup based on table
 	if (table == 'users') {
-		/**
-		 * LIVE DEBUGGING
-		 */
-		/* if (e.model.get('callsign') != 'KF7CVT2') {
-			return
-		} */
-	
 		// only go through this process if the user is being updated from ready:false to ready:true
 		if (current_model.get('ready') != false || e.model.get('ready') != true) {
 			return
 		}
-	
+
 		// check if there's an active exercise
 		try {
 			exercise_record = $app.dao().findFirstRecordByFilter(
@@ -26,7 +19,7 @@ onModelBeforeUpdate((e) => {
 				{ now: new Date().toISOString().replace('T', ' ').substr(0, 19) }
 			)
 		} catch (error) { /* no active exercises */}
-	
+
 		// stop if there's no active exercise
 		if (exercise_record == undefined) {
 			return
@@ -50,7 +43,7 @@ onModelBeforeUpdate((e) => {
 		if (current_model.get('resource_rejected') != false || e.model.get('resource_rejected') != true) {
 			return
 		}
-	
+
 		// check if there's an active exercise
 		try {
 			exercise_record = $app.dao().findFirstRecordByFilter(
@@ -59,7 +52,7 @@ onModelBeforeUpdate((e) => {
 				{ now: new Date().toISOString().replace('T', ' ').substr(0, 19) }
 			)
 		} catch (error) { /* no active exercises */}
-	
+
 		// stop if there's no active exercise
 		if (exercise_record == undefined) {
 			return
@@ -73,81 +66,73 @@ onModelBeforeUpdate((e) => {
 	}
 
 	// shoring up
-	const id_model = new DynamicModel({ "id": "" })
-	const stake_weight = exercise_record.get('stake_distribution')
-	const region_weight = exercise_record.get('region_distribution')
-	const storehouse_weight = exercise_record.get('storehouse_distribution')
 	const tasks_collection = $app.dao().findCollectionByNameOrId("tasks")
+	const items = new DynamicModel({ "count": 0 })
+	$app.dao().db().newQuery("SELECT COUNT(id) AS count FROM items;").one(items)
+
+	function chooseUser(user_id) {
+		const eligible_users = arrayOf(new DynamicModel({ "id": "" }))
+		const stake_weight = exercise_record.get('stake_distribution')
+		const region_weight = exercise_record.get('region_distribution')
+		const storehouse_weight = exercise_record.get('storehouse_distribution')
+		const scope_total = stake_weight + region_weight + storehouse_weight
+		const user_model = $app.dao().findRecordById('users', user_id)
+		const region_id = $app.dao().findRecordById("stakes", user_model.get('stake')).get('region')
+
+		// choose random scope
+		do {
+			var scope_chooser = Math.random() * scope_total,
+				i = 0
+			if (scope_chooser <= stake_weight) {
+				// build list of eligible resource users from the stake
+				$app.dao().db()
+					.select('users.id')
+					.from('users')
+					.where($dbx.exp("stake = {:stake} AND id != {:id}", {
+						stake: user_model.get('stake'),
+						id: user_id
+					}))
+					.orderBy('users.id')
+					.all(eligible_users)
+			} else if (scope_chooser <= region_weight + stake_weight) {
+				// build list of eligible resource users from the region
+				$app.dao().db()
+					.select('users.id')
+					.from('users')
+					.leftJoin('stakes', $dbx.exp("users.stake = stakes.id"))
+					.where($dbx.exp("stakes.region = {:region} AND users.stake != {:stake}", {
+						region: region_id,
+						stake: user_model.get('stake')
+					}))
+					.orderBy('users.id')
+					.all(eligible_users)
+			} else {
+				// build list of eligible resource users from the storehouse
+				$app.dao().db()
+					.select('users.id')
+					.from('users')
+					.leftJoin('stakes', $dbx.exp("users.stake = stakes.id"))
+					.where($dbx.exp("stakes.region != {:region}", {
+						region: region_id
+					}))
+					.orderBy('users.id')
+					.all(eligible_users)
+			}
+		} while (eligible_users.length == 0 && i++ < 3)
+
+		// sanity check to make sure there's enough users
+		if (eligible_users.length == 0) {
+			console.error("chooseUser() logic error! There doesn't seem to be any other users available to use as the resource user!")
+			throw new ApiError(500, 'Unable to find a resource user!')
+		}
+
+		// pick random resource user
+		const user_chooser = Math.floor(Math.random() * eligible_users.length)
+		return eligible_users[user_chooser].id
+	}
 
 	// loop through processing list
 	user_ids.forEach(user_id => {
-		var eligible_users = [];
-		var user_model = $app.dao().findRecordById('users', user_id)
-
-		// build list of eligible resource users from the stake
-		var stake_users = arrayOf(id_model)
-		$app.dao().db()
-			.select('users.id')
-			.from('users')
-			.where($dbx.exp("stake = {:stake} AND id != {:id}", {
-				stake: user_model.get('stake'),
-				id: user_id
-			}))
-			.all(stake_users)
-		stake_users.forEach(stake_user => {
-			eligible_users.push({
-				id: stake_user.id,
-				weight: stake_weight
-			})
-		})
-	
-		// build list of eligible resource users from the region
-		var region_id = $app.dao().findRecordById("stakes", user_model.get('stake')).get('region')
-		var region_users = arrayOf(new DynamicModel({ "id": "" }))
-		$app.dao().db()
-			.select('users.id')
-			.from('users')
-			.leftJoin('stakes', $dbx.exp("users.stake = stakes.id"))
-			.where($dbx.exp("stakes.region = {:region} AND users.stake != {:stake}", {
-				region: region_id,
-				stake: user_model.get('stake')
-			}))
-			.all(region_users)
-		region_users.forEach(region_user => {
-			eligible_users.push({
-				id: region_user.id,
-				weight: region_weight
-			})
-		})
-	
-		// build list of eligible resource users from the region
-		var storehouse_users = arrayOf(new DynamicModel({ "id": "" }))
-		$app.dao().db()
-			.select('users.id')
-			.from('users')
-			.leftJoin('stakes', $dbx.exp("users.stake = stakes.id"))
-			.where($dbx.exp("stakes.region != {:region}", {
-				region: region_id
-			}))
-			.all(storehouse_users)
-		storehouse_users.forEach(storehouse_user => {
-			eligible_users.push({
-				id: storehouse_user.id,
-				weight: storehouse_weight
-			})
-		})
-	
-		// shuffle list
-		const weight_max = (stake_users.length * stake_weight)
-			+ (region_users.length * region_weight)
-			+ (storehouse_users.length * storehouse_weight)
-		for (var i = eligible_users.length - 1; i > 0; i--) {
-			var j = Math.floor(Math.random() * (i + 1));
-			var temp = eligible_users[i];
-			eligible_users[i] = eligible_users[j];
-			eligible_users[j] = temp;
-		}
-	
 		// create or update based on update type
 		if (table == 'users' || table == 'exercises') {
 			// check users existing tasks
@@ -158,29 +143,29 @@ onModelBeforeUpdate((e) => {
 					"need_user": user_id
 				})
 				.one(tasks)
-		
+
 			// create multiple tasks based on previous task count
-			var new_task_count = tasks.count > 0 ? 1 : 3
+			var new_task_count = tasks.count > 0 ? 10 : 3
 			for (let j = 0; j < new_task_count; j++) {
 				// pick a random task
 				var task = new DynamicModel({ "id": "" })
 				$app.dao().db()
-					.newQuery("SELECT id\
+					.newQuery('SELECT items.id, COUNT(tasks.id) AS used\
 						FROM items\
-						ORDER BY RANDOM()\
-						LIMIT 1;")
+						LEFT JOIN tasks ON tasks.item = items.id\
+						GROUP BY items.id\
+						ORDER BY used ASC, RANDOM()\
+						LIMIT 1')
+					/* .select('id')
+					.from('items')
+					.orderBy('id')
+					.limit(1)
+					.offset(Math.floor(Math.random() * items.count)) */
 					.one(task)
-		
-				// pick random resource user
-				var pointer = Math.random() * weight_max
-				var weight_sum = 0
-				var ii = 0
-				while (weight_sum < pointer && ii < eligible_users.length) {
-					weight_sum = weight_sum + eligible_users[ii].weight
-					ii++
-				}
-				var resource_user = eligible_users[ii - 1].id
-		
+
+				// pick resource user
+				var resource_user = chooseUser(user_id)
+
 				// create task
 				$app.dao().saveRecord(new Record(tasks_collection, {
 					"need_user": user_id,
@@ -190,15 +175,8 @@ onModelBeforeUpdate((e) => {
 				}))
 			}
 		} else if (table == 'tasks') {
-			// pick random resource user
-			var pointer = Math.random() * weight_max
-			var weight_sum = 0
-			var ii = 0
-			while (weight_sum < pointer && ii < eligible_users.length) {
-				weight_sum = weight_sum + eligible_users[ii].weight
-				ii++
-			}
-			var resource_user = eligible_users[ii - 1].id
+			// pick resource user
+			var resource_user = chooseUser(user_id)
 
 			// update existing task with new user
 			e.model.set('resource_user', resource_user)
