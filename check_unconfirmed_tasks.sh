670486423aa32dc1abe2185ff7c699f368eb8273
Caleb Hornbeck <inventor96@gmail.com>
2025-06-14 11:26:38 -0600

    Improved readability

--- check_unconfirmed_tasks.sh
+++ check_unconfirmed_tasks.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 while true; do
 	sleep 15
-	curl -s http://127.0.0.1:8090/check-unconfirmed-tasks
+	date
+	echo ""
+	curl 'http://127.0.0.1:8090/check-unconfirmed-tasks'
+	echo ""
 done
