095ef0f5c6baaad2fa647d2e08bffdb597470d6d
Caleb Hornbeck <inventor96@gmail.com>
2025-06-16 16:24:08 -0600

    Update collection identifiers

--- pb_migrations/1749251472_updated_items.js
+++ pb_migrations/1749251472_updated_items.js
@@ -54,7 +54,7 @@ migrate((db) => {
   return dao.saveCollection(collection)
 }, (db) => {
   const dao = new Dao(db)
-  const collection = dao.findCollectionByNameOrId("f18rsozza947st7")
+  const collection = dao.findCollectionByNameOrId("items")
 
   // update
   collection.schema.addField(new SchemaField({
